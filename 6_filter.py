6c66fcbc649678396d30cae7809dcd79b676e260
iamakawa <theiooori@gmail.com>
2019-01-03 13:51:57 +0900

    6_fillter

--- 6_filter.py
+++ 6_filter.py
@@ -9,6 +9,7 @@ cv2.namedWindow('dst_laplacian', cv2.WINDOW_NORMAL)
 
 img_src = cv2.imread(file_src, 1)
 
+# フィルタ処理
 img_dst_weightedave = cv2.blur(img_src, (3, 3))
 img_dst_gaussian    = cv2.GaussianBlur(img_src, (11, 11), 1)
 img_dst_laplacian   = cv2.Laplacian(img_src, cv2.CV_32F, 3)
